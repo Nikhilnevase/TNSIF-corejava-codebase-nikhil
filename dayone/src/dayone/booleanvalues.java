967310eece2ecd13e81ec04f235d5c67ffd43060
Nikhil <Nikhil@LAPTOP-92I93TOV>
2025-06-30 20:28:37 +0530

    booleans

---
+++ dayone/src/dayone/booleanvalues.java
@@ -0,0 +1,10 @@
+package dayone;
+
+public class booleanvalues {
+	public static void  main (String[]args) {}
+	// Boolean is the data type use to store the True or False
+	boolean a = true;
+	boolean b = false;
+	
+
+}
--- dayone1/src/day/operators.java
+++ dayone1/src/day/operators.java
@@ -1,5 +1,21 @@
 package day;
 
 public class operators {
+	 public static void main (String[]args) {
+		 // operators are  mathematical symboles  that use between two operands to perform specific task
+		 // 1 increament operator 
+		 int a = 1;
+		 a=a++;    //post increament 
+		 System.out.println(a);
+		 ++a;      // pre increament 
+		 System.out.println(a);
+		 
+		 // decreament operator 
+		 
+		 --a;   //pre decreament 
+		 System.out.println(a);
+		 a--;
+		 System.out.println(a);
+	 }
 
 }
