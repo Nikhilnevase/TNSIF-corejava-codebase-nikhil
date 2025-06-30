4df84c498996d1193eb1527b19a3764437d1fe84
Nikhil <Nikhil@LAPTOP-92I93TOV>
2025-06-30 19:56:35 +0530

    decimals

---
+++ dayone/src/dayone/decimals.java
@@ -0,0 +1,15 @@
+package dayone;
+
+public class decimals {
+	public static void main(String [] args) {
+		//float is data type use store the decimal number with less roundoff 
+		//example
+		float a = 10f/3f;
+		System.out.println(a);
+		// double is the data type use to store the decimals with large roundoff 
+		double b = 10d/3d;
+		System.out.println(b);
+		
+	}
+
+}
--- dayone/src/dayone/javabasics.java
+++ dayone/src/dayone/javabasics.java
@@ -2,6 +2,7 @@ package dayone;
 
 public class javabasics {
 	public static void main(String[] args) {
+		//DataTypes
 		int a = 12;
 		System.out.println(a);
 		short b = 36;
@@ -9,9 +10,9 @@ public class javabasics {
 		byte c = 78;
 		System.out.println(c);
 		long d = 98830234777578388L;
-		
-	
 		System.out.println(d);
+	
+		
 		
 		
 	
