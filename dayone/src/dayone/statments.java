64ad9426fb082563f6642fbfa123779d2574dd3c
Nikhil <Nikhil@LAPTOP-92I93TOV>
2025-06-30 20:43:50 +0530

    condition_statments

---
+++ dayone/src/dayone/statments.java
@@ -0,0 +1,17 @@
+package dayone;
+
+public class statments {
+	public static void main (String []args) {
+		// condition statments are use to perform the task when specified condition has occures
+		// if else statment 
+		int a = 10;
+		if (a==10) {
+			System.out.print(a);
+		} else {
+		System.out.print("a is not equal to  10");
+		}
+	}
+
+}
+
+	
