e0edb47fe868dfb6850408374dd8df766875a986
Nikhil <Nikhil@LAPTOP-92I93TOV>
2025-07-03 11:18:21 +0530

    Demo1

--- tnsdayfour/src/AcceseMobifiers/privateDemo1.java
+++ tnsdayfour/src/AcceseMobifiers/privateDemo1.java
@@ -1,7 +1,26 @@
 package AcceseMobifiers;
 
 public class privateDemo1 {
+	public static void main(String[]args ) {
 	modifierpublic obj = new modifierpublic ();
-	  obj.setvarpublic();
+		obj.methodpublic();
+		obj.varpublic=10;
+		obj.methodpublic();
+		
+		obj.methodprotected();
+		obj.varprotected=20;
+		obj.methodprotected();
+		
+		obj.methodDefault();
+		obj.vardefault=30;
+		obj.methodDefault();
+		
+	}
+	
+	
+	
+	
+	
+	  
 
 }
